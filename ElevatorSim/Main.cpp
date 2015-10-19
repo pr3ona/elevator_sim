63026b7ba440bf2282ca9bc4a58786e5ed9ba87c
THEO457 <theolen.naidoo@rocketmail.com>
2015-10-19 15:37:23 +0200

    Initial Commit Allegro

---
+++ ElevatorSim/Main.cpp
@@ -0,0 +1,10 @@
+#include <allegro5\allegro5.h>
+#include <allegro5\allegro_primitives.h>
+#include <allegro5\allegro_image.h>
+#include <cmath>
+#include <time.h>
+#include <allegro5\allegro_font.h>
+#include <allegro5\allegro_ttf.h>
+#include <allegro5\allegro_audio.h> 
+#include <allegro5\allegro_acodec.h> 
+#include <stdio.h>
