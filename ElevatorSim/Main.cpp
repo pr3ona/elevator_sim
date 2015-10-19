47c2be53b9b00309dbb2f79fecda2c87cfb4e1df
THEO457 <theolen.naidoo@rocketmail.com>
2015-10-19 16:17:54 +0200

    Initialize Allegro

--- ElevatorSim/Main.cpp
+++ ElevatorSim/Main.cpp
@@ -1,8 +1,8 @@
-#include <allegro5\allegro5.h>
+#include <iostream>
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_native_dialog.h>
 #include <allegro5\allegro_primitives.h>
 #include <allegro5\allegro_image.h>
-#include <cmath>
-#include <time.h>
 #include <allegro5\allegro_font.h>
 #include <allegro5\allegro_ttf.h>
 #include <allegro5\allegro_audio.h> 
@@ -10,3 +10,29 @@
 #include <stdio.h>
 
 using namespace std;
+
+int screenH = 800;
+int screenW = 600;
+
+int main()
+{
+	/*INITIALIZE ALLEGRO 5*/
+	al_init();
+	if (!al_init()) 
+	{
+		al_show_native_message_box(0, "ERROR", "Failed to initialize Allegro 5!", "Press OK to terminate.", 0, ALLEGRO_MESSAGEBOX_ERROR);
+		return -1;
+	}
+
+	/*CREATE DISPLAY*/
+	ALLEGRO_DISPLAY *display;
+	display = al_create_display(screenW, screenW);
+	al_set_window_position(display, 200, 200);
+	if (!display) 
+	{
+		al_show_native_message_box(0, "ERROR", "Failed to initialize the display!", "Press OK to terminate.", 0, ALLEGRO_MESSAGEBOX_ERROR);
+		return -1;
+	}
+
+	return 0;
+}
