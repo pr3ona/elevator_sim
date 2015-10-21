c9d907459761ed4c5b66323950faa95b6c191fd3
pr3ona <preona@gmail.com>
2015-10-21 10:06:35 +0200

    mouse click

--- ElevatorSim/Main.cpp
+++ ElevatorSim/Main.cpp
@@ -1,38 +1,66 @@
-#include <iostream>
-#include <allegro5/allegro.h>
-#include <allegro5/allegro_native_dialog.h>
-#include <allegro5\allegro_primitives.h>
-#include <allegro5\allegro_image.h>
-#include <allegro5\allegro_font.h>
-#include <allegro5\allegro_ttf.h>
-#include <allegro5\allegro_audio.h> 
-#include <allegro5\allegro_acodec.h> 
-#include <stdio.h>
+#include <allegro5\allegro.h>
+#include <allegro5\allegro_primitives.h>				//Our primitive header file
 
-using namespace std;
-///
-int screenH = 800;
-int screenW = 600;
-
-int main()
+int main(void)
 {
-	/*INITIALIZE ALLEGRO 5*/
-	al_init();
-	if (!al_init()) 
-	{
-		al_show_native_message_box(0, "ERROR", "Failed to initialize Allegro 5!", "Press OK to terminate.", 0, ALLEGRO_MESSAGEBOX_ERROR);
+	int width = 640;
+	int height = 480;
+
+	bool done = false;
+	bool draw = true;
+	int pos_x = width / 2;
+	int pos_y = height / 2;
+
+	ALLEGRO_DISPLAY *display = NULL;
+	ALLEGRO_EVENT_QUEUE *event_queue = NULL;
+
+	if (!al_init())										//initialize Allegro
 		return -1;
+
+	display = al_create_display(width, height);			//create our display object
+
+	if (!display)										//test display object
+		return -1;
+
+	al_init_primitives_addon();
+	al_install_mouse();
+
+	event_queue = al_create_event_queue();
+
+	al_register_event_source(event_queue, al_get_mouse_event_source());
+	al_register_event_source(event_queue, al_get_display_event_source(display));
+
+	al_hide_mouse_cursor(display);
+	while (!done)
+	{
+		ALLEGRO_EVENT ev;
+		al_wait_for_event(event_queue, &ev);
+
+		if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
+		{
+			done = true;
+		}
+		else if (ev.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN)
+		{
+			if (ev.mouse.button & 1)
+				draw = !draw;
+			else if (ev.mouse.button & 2)
+				done = true;
+		}
+		else if (ev.type == ALLEGRO_EVENT_MOUSE_AXES)
+		{
+			pos_x = ev.mouse.x;
+			pos_y = ev.mouse.y;
+		}
+
+		if (draw && pos_x<200)
+			al_draw_filled_rectangle(pos_x, pos_y, pos_x + 30, pos_y + 30, al_map_rgb(255, 0, 255));
+		al_flip_display();
+		al_clear_to_color(al_map_rgb(0, 0, 0));
 	}
 
-	/*CREATE DISPLAY*/
-	ALLEGRO_DISPLAY *display;
-	display = al_create_display(screenW, screenW);
-	al_set_window_position(display, 200, 200);
-	if (!display) 
-	{
-		al_show_native_message_box(0, "ERROR", "Failed to initialize the display!", "Press OK to terminate.", 0, ALLEGRO_MESSAGEBOX_ERROR);
-		return -1;
-	}
+	al_destroy_event_queue(event_queue);
+	al_destroy_display(display);						//destroy our display object
 
 	return 0;
 }
