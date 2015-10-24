f827ac6b8cf62f3e401d543f0a38cf9f91f2f8ff
THEO457 <theolen.naidoo@rocketmail.com>
2015-10-24 15:22:12 +0200

    MovinDot

--- ElevatorSim/Main.cpp
+++ ElevatorSim/Main.cpp
@@ -7,32 +7,41 @@
 #include <allegro5\allegro_primitives.h>
 #include <allegro5\allegro_font.h>
 #include <allegro5\allegro_ttf.h>
+#include "Elevator.h"
+
+const int NumFloors = 4;
+
+void InitElevator(Elevatorm elevatorm[], int size);
+void DrawElevator(Elevatorm elevatorm[], int size);
+void FireElevator(Elevatorm elevatorm[], int size);
+void UpdateElevator(Elevatorm elevatorm[], int size);
 
 int main(void)
 {
+	Elevatorm elevatorm[5];
+
 	int width = 900;
 	int height = 700;
 
+	const int FPS = 60;
 	bool done = false;
 	bool draw = true;
 	int pos_x = width / 2;
 	int pos_y = height / 2;
 
 	int liftH = 0;
-	int liftW = 0; 
+	int liftW = 0;
 	bool floor2 = false;
 
-		ALLEGRO_DISPLAY *display = NULL;
+	ALLEGRO_DISPLAY *display = NULL;
 	ALLEGRO_EVENT_QUEUE *event_queue = NULL;
 	ALLEGRO_BITMAP *But1 = NULL;
 	ALLEGRO_BITMAP *But2 = NULL;
 	ALLEGRO_BITMAP *But3 = NULL;
 	ALLEGRO_BITMAP *But4 = NULL;
 	ALLEGRO_BITMAP *ButUp1 = NULL;
-	ALLEGRO_BITMAP *ButUp2= NULL;
+	ALLEGRO_BITMAP *ButUp2 = NULL;
 	ALLEGRO_BITMAP *ButUp3 = NULL;
-	ALLEGRO_BITMAP *ButUp4 = NULL;
-	ALLEGRO_BITMAP *ButDown1 = NULL;
 	ALLEGRO_BITMAP *ButDown2 = NULL;
 	ALLEGRO_BITMAP *ButDown3 = NULL;
 	ALLEGRO_BITMAP *ButDown4 = NULL;
@@ -53,10 +62,11 @@ int main(void)
 	ALLEGRO_BITMAP *Elevator = NULL;
 
 	ALLEGRO_SAMPLE *sample = NULL;
+	ALLEGRO_SAMPLE *alarm = NULL;
 
+	ALLEGRO_TIMER *timer = NULL;
+	ALLEGRO_EVENT ev;
 
-
-	
 	if (!al_init())										//initialize Allegro
 		return -1;
 
@@ -83,7 +93,7 @@ int main(void)
 	Elevator = al_load_bitmap("Elevator.png");
 	LiftOpen = al_load_bitmap("LiftOpen.png");
 	Liftclose = al_load_bitmap("LiftClose.png");
-	
+
 	But1 = al_load_bitmap("ElevatorButton1.png");
 	But2 = al_load_bitmap("ElevatorButton2.png");
 	But3 = al_load_bitmap("ElevatorButton3.png");
@@ -92,9 +102,7 @@ int main(void)
 	ButUp1 = al_load_bitmap("ElevatorButtonUp.png");
 	ButUp2 = al_load_bitmap("ElevatorButtonUp.png");
 	ButUp3 = al_load_bitmap("ElevatorButtonUp.png");
-	ButUp4 = al_load_bitmap("ElevatorButtonUp.png");
-
-	ButDown1 = al_load_bitmap("ElevatorButtonDown.png");
+	
 	ButDown2 = al_load_bitmap("ElevatorButtonDown.png");
 	ButDown3 = al_load_bitmap("ElevatorButtonDown.png");
 	ButDown4 = al_load_bitmap("ElevatorButtonDown.png");
@@ -104,24 +112,30 @@ int main(void)
 	Close = al_load_bitmap("ElevatorButtonClose.png");
 	Alarm = al_load_bitmap("ElevatorButtonA.png");
 	Panel1 = al_load_bitmap("MetalPlate2.png");
-	Panel2= al_load_bitmap("MetalPlate2.png");
-	panel3= al_load_bitmap("MetalPlate2.png");
-	Panel4= al_load_bitmap("MetalPlate2.png");
+	Panel2 = al_load_bitmap("MetalPlate2.png");
+	panel3 = al_load_bitmap("MetalPlate2.png");
+	Panel4 = al_load_bitmap("MetalPlate2.png");
 
 	sample = al_load_sample("Music.wav");
+	alarm = al_load_sample("Alarm.wav");
 
 	liftH = al_get_bitmap_height(Liftclose);
 	liftW = al_get_bitmap_width(Liftclose);
-	
+
 	int x = 440 - liftW / 2;
 	int y = 700 - liftH - 100;
 
+	InitElevator(elevatorm, NumFloors);
+	
 	event_queue = al_create_event_queue();
-
+	timer = al_create_timer(1.0 / FPS);
 	al_register_event_source(event_queue, al_get_mouse_event_source());
+	al_register_event_source(event_queue, al_get_timer_event_source(timer));
 	al_register_event_source(event_queue, al_get_display_event_source(display));
 
+
 	//al_hide_mouse_cursor(display);
+	al_start_timer(timer);
 	while (!done)
 	{
 		al_draw_line(0, 150, 600, 150, al_map_rgb(255, 0, 0), 2);
@@ -129,15 +143,15 @@ int main(void)
 		al_draw_line(0, 450, 600, 450, al_map_rgb(255, 0, 0), 2);
 		al_draw_line(0, 600, 600, 600, al_map_rgb(255, 0, 0), 2);
 		al_draw_line(465, 0, 465, 700, al_map_rgb(121, 121, 121), 200);
-		al_draw_bitmap(Elevator, x, y, 0);
+		//al_draw_bitmap(Elevator, x, y, 0);
 
 		al_draw_bitmap(Liftclose, 250 - liftW / 2, 700 - liftH - 100, 0);
 		al_draw_bitmap(Liftclose, 250 - liftW / 2, 550 - liftH - 100, 0);
 		al_draw_bitmap(Liftclose, 250 - liftW / 2, 400 - liftH - 100, 0);
 		al_draw_bitmap(Liftclose, 250 - liftW / 2, 250 - liftH - 100, 0);
-		al_draw_bitmap(Plate, 600, 0,0);
+		al_draw_bitmap(Plate, 600, 0, 0);
 
-		al_draw_bitmap(But1,610,75,0);
+		al_draw_bitmap(But1, 610, 75, 0);
 		al_draw_bitmap(But2, 750, 75, 0);
 
 		al_draw_bitmap(But3, 610, 140, 0);
@@ -156,26 +170,30 @@ int main(void)
 		al_play_sample(sample, 1.0, 0.0, 1.0, ALLEGRO_PLAYMODE_LOOP, NULL);
 		//al_rest(10.0);
 
-		al_draw_bitmap(Panel1,0, 450, 0);
+		al_draw_bitmap(Panel1, 0, 450, 0);
 		al_draw_bitmap(ButUp1, 10, 450, 0);
-		al_draw_bitmap(ButDown1, 10, 510, 0);
-
+		
 		al_draw_bitmap(Panel1, 0, 300, 0);
 		al_draw_bitmap(ButUp2, 10, 300, 0);
-		al_draw_bitmap(ButDown2, 10, 360,0);
+		al_draw_bitmap(ButDown2, 10, 360, 0);
 
 		al_draw_bitmap(Panel1, 0, 150, 0);
 		al_draw_bitmap(ButUp3, 10, 140, 0);
 		al_draw_bitmap(ButDown3, 10, 200, 0);
-		
-		al_draw_bitmap(Panel1, 0,0, 0);
-		al_draw_bitmap(ButUp4, 10, 0,0); 
-		al_draw_bitmap(ButDown4, 10, 60, 0);
-							
-		ALLEGRO_EVENT ev;
-		al_wait_for_event(event_queue, &ev);
 
-		if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
+		al_draw_bitmap(Panel1, 0, 0, 0);
+		al_draw_bitmap(ButDown4, 10, 60, 0);
+
+		//ALLEGRO_EVENT ev;
+		al_wait_for_event(event_queue, &ev);
+		if (ev.type == ALLEGRO_EVENT_TIMER)
+		{
+			draw = true;
+
+			UpdateElevator(elevatorm, NumFloors);
+
+		}
+		else if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
 		{
 			done = true;
 		}
@@ -183,7 +201,10 @@ int main(void)
 		{
 
 			if (ev.mouse.button & 1 && pos_x > 630 && pos_x < 720 && pos_y >90 && pos_y < 140)
-				al_draw_bitmap(LiftOpen, 250 - liftW / 2, 700 - liftH - 100, 0);// but1
+			{
+				al_draw_bitmap(LiftOpen, 250 - liftW / 2, 700 - liftH - 100, 0);
+				//al_rest(5);
+			}// but1
 
 			else if (ev.mouse.button & 1 && pos_x>770 && pos_x < 860 && pos_y>90 && pos_y < 140)
 			{
@@ -194,55 +215,53 @@ int main(void)
 				al_rest(5);
 			} //but2
 
-			else if (ev.mouse.button & 1 && pos_x>630 && pos_x<720 && pos_y >160 && pos_y < 210)
+			else if (ev.mouse.button & 1 && pos_x>630 && pos_x < 720 && pos_y >160 && pos_y < 210)
 			{
 				al_draw_bitmap(LiftOpen, 250 - liftW / 2, 400 - liftH - 100, 0);
 				al_draw_bitmap(Black, x, y, 0);
 				al_draw_bitmap(Elevator, x, y - 300, 1);
 				al_flip_display();
-				al_rest(5);
+				//al_rest(5);
 			} // but3
 
-			else if (ev.mouse.button & 1 && pos_x>770 && pos_x<860 && pos_y>160 && pos_y<210)
+			else if (ev.mouse.button & 1 && pos_x>770 && pos_x < 860 && pos_y>160 && pos_y < 210)
 			{
-				al_draw_bitmap(LiftOpen, 250 - liftW / 2, 250 - liftH - 100, 0);
+				FireElevator(elevatorm, NumFloors,);
+				
+				/*al_draw_bitmap(LiftOpen, 250 - liftW / 2, 250 - liftH - 100, 0);
 				al_draw_bitmap(Black, x, y, 0);
 				al_draw_bitmap(Elevator, x, y - 450, 1);
 				al_flip_display();
-				al_rest(5);
+				al_rest(5);*/
 			} // but4
 
-			else if (ev.mouse.button & 1 && pos_x>630 && pos_x<720 && pos_y >230 && pos_y < 280)
+			else if (ev.mouse.button & 1 && pos_x>630 && pos_x < 720 && pos_y >230 && pos_y < 280)
 				done = true;// open
 
-			else if (ev.mouse.button & 1 && pos_x>770 && pos_x<860 && pos_y>230&& pos_y<280)
+			else if (ev.mouse.button & 1 && pos_x>770 && pos_x < 860 && pos_y>230 && pos_y < 280)
 				done = true;// close
 
-			else if (ev.mouse.button & 1 && pos_x>700 && pos_x<800 && pos_y> 295 && pos_y<345)
-				done = true;// Alarm
+			else if (ev.mouse.button & 1 && pos_x>700 && pos_x < 800 && pos_y> 295 && pos_y < 345)
+			{
+				//al_play_sample_instance(alarm);// Alarm
+			}
 
-			else if (ev.mouse.button & 1 && pos_x>20 && pos_x<125 && pos_y> 470 && pos_y<515)
+			else if (ev.mouse.button & 1 && pos_x>20 && pos_x < 125 && pos_y> 470 && pos_y < 515)
 				done = true;// up1
 
-			else if (ev.mouse.button & 1 && pos_x>20 && pos_x<125 && pos_y> 530 && pos_y<575)
-				done = true;// down1
-
-			else if (ev.mouse.button & 1 && pos_x>20 && pos_x<125 && pos_y> 320 && pos_y<365)
+			else if (ev.mouse.button & 1 && pos_x>20 && pos_x < 125 && pos_y> 320 && pos_y < 365)
 				done = true;// up2
 
-			else if (ev.mouse.button & 1 && pos_x>20 && pos_x<125 && pos_y> 380 && pos_y<425)
+			else if (ev.mouse.button & 1 && pos_x>20 && pos_x < 125 && pos_y> 380 && pos_y < 425)
 				done = true;// down2
 
-			else if (ev.mouse.button & 1 && pos_x>20 && pos_x<125 && pos_y> 160 && pos_y<205)
+			else if (ev.mouse.button & 1 && pos_x>20 && pos_x < 125 && pos_y> 160 && pos_y < 205)
 				done = true;// up3
 
-			else if (ev.mouse.button & 1 && pos_x>20 && pos_x<125 && pos_y> 220 && pos_y<265)
+			else if (ev.mouse.button & 1 && pos_x>20 && pos_x < 125 && pos_y> 220 && pos_y < 265)
 				done = true;// down3
 
-			else if (ev.mouse.button & 1 && pos_x>20 && pos_x<125 && pos_y> 25 && pos_y<70)
-				done = true;// up4
-
-			else if (ev.mouse.button & 1 && pos_x>20 && pos_x<125 && pos_y> 85 && pos_y<130)
+			else if (ev.mouse.button & 1 && pos_x>20 && pos_x < 125 && pos_y> 85 && pos_y < 130)
 				done = true;// down4
 
 
@@ -254,18 +273,25 @@ int main(void)
 			pos_x = ev.mouse.x;
 			pos_y = ev.mouse.y;
 		}
-
-	/*		while (floor2 && liftH != 115)
-			{
+		/*		while (floor2 && liftH != 115)
+				{
 				liftH + 10;
 				al_draw_bitmap(Liftclose, 400 - liftW / 2, 700 - liftH - 100, 0);
-			}// but2
-		
-		/*if (draw && pos_x<200)
-			al_draw_filled_rectangle(pos_x, pos_y, pos_x + 30, pos_y + 30, al_map_rgb(255, 0, 255));*/
+				}// but2
+
+				/*if (draw && pos_x<200)
+				al_draw_filled_rectangle(pos_x, pos_y, pos_x + 30, pos_y + 30, al_map_rgb(255, 0, 255));*/
 		al_flip_display();
 		al_clear_to_color(al_map_rgb(0, 0, 0));
-	}
+		if (draw && al_is_event_queue_empty(event_queue))
+		{
+			draw = false;
+         	DrawElevator(elevatorm, NumFloors);
+
+			al_flip_display();
+			al_clear_to_color(al_map_rgb(0, 0, 0));
+		}
+}
 	al_destroy_bitmap(But1);
 	al_destroy_bitmap(But2);
 	al_destroy_bitmap(But3);
@@ -273,8 +299,6 @@ int main(void)
 	al_destroy_bitmap(ButUp1);
 	al_destroy_bitmap(ButUp2);
 	al_destroy_bitmap(ButUp3);
-	al_destroy_bitmap(ButUp4);
-	al_destroy_bitmap(ButDown1);
 	al_destroy_bitmap(ButDown2);
 	al_destroy_bitmap(ButDown3);
 	al_destroy_bitmap(ButDown4);
@@ -284,8 +308,59 @@ int main(void)
 
 	al_destroy_sample(sample);
 
-	al_destroy_event_queue(event_queue);
-	al_destroy_display(display);						//destroy our display object
 
-	return 0;
+	al_destroy_event_queue(event_queue);
+	if (ev.type == ALLEGRO_EVENT_TIMER)
+	{
+		draw = true;
+		al_destroy_display(display);						//destroy our display object
+
+		return 0;
+	}					//destroy our display object
+}
+
+void InitElevator(Elevatorm elevatorm[], int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		elevatorm[i].ID = ELEVATORM;
+		elevatorm[i].speed = 10;
+		elevatorm[i].up = false;
+	}
+}
+
+void DrawElevator(Elevatorm elevatorm[], int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		if (elevatorm[i].up)
+		{
+			al_draw_filled_circle(elevatorm[i].x, elevatorm[i].y, 5, al_map_rgb(255, 0, 0));
+		}
+			}
+}
+void FireElevator(Elevatorm elevatorm[], int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		if (!elevatorm[i].up)
+		{
+			elevatorm[i].x = 100;
+			elevatorm[i].y = 100;
+			elevatorm[i].up = true;
+			break;
+		}
+	}
+}
+void UpdateElevator(Elevatorm elevatorm[], int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		if (elevatorm[i].up)
+		{
+			elevatorm[i].x += elevatorm[i].speed;
+			if (elevatorm[i].x > 200)
+				elevatorm[i].up = 200;
+		}
+	}
 }
